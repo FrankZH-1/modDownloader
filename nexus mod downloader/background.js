5ff77393ebb8bd199394738ab22415cfa3bbd414
FrankZH-1 <chinacpld@126.com>
2023-04-28 01:08:44 +0800

    尝试每步均发起xhr

--- nexus mod downloader/background.js
+++ nexus mod downloader/background.js
@@ -3,29 +3,68 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 let urls = [];
+let id = [];
+let count = 0;
+let current = 0;
+let urlList = [];
+const gameId = { monsterhunterrise: 4095 };
+const urlHead =
+  "https://www.nexusmods.com/Core/Libs/Common/Managers/Downloads?GenerateDownloadUrl";
 
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     if (request.action === "getTabUrls") {
-      chrome.tabs.query({}, (tabs) => {
-        const filteredTabs = tabs.filter(
-          (tab) => {
-            return /^https?:\/\/(www\.)?nexusmods\.com/.test(
-              tab.url
-            );
-          }
-        );
-        filteredTabs.forEach((element) => {
-          urls.push(element.url);
-        });
-      });
-      console.log("got your quest~");
+      sendResponse(filterUrl())
     }
-    if (urls.length) {
-      sendResponse({
-        outcome: urls,
-      });
-      urls = [];
-    } else sendResponse({ outcome: "failed" });
+    else if (request.action === 'download') {
+      {
+        sendResponse(download(request.data))
+        // sendResponse({ response: "123" })
+      }
+    }
   }
 );
+
+function filterUrl() {
+  chrome.tabs.query({}, (tabs) => {
+    const filteredTabs = tabs.filter(
+      (tab) => {
+        return /^https?:\/\/(www\.)?nexusmods\.com/.test(
+          tab.url
+        );
+      }
+    );
+    filteredTabs.forEach((element) => {
+      urls.push(element.url);
+    });
+  });
+  console.log("got your quest~");
+  count = urls.length
+  if (urls.length)
+    return { outcome: urls };
+  else return { outcome: "failed" }
+}
+
+function gatherDownloadUrl(data) {
+  urlList.concat(data)
+}
+
+
+
+function download(data) {
+  console.log(current);
+
+  if (current < count) {
+    gatherDownloadUrl(data)
+    current++;
+  }
+  else {
+    count = 0
+    startDonwload()
+  }
+}
+
+function startDonwload() {
+  console.log("download started", urlList)
+  return { response: "download started" }
+}
--- nexus mod downloader/js/script.js
+++ nexus mod downloader/js/script.js
@@ -15,55 +15,13 @@ document.addEventListener(
           response.outcome.forEach((item) => {
             getPage(item);
           });
-          if (response.outcome != "failed") {
-            handleUrl(response.outcome);
-          }
         }
       );
     });
   }
 );
 
-function handleUrl(urls) {
-  // const regex = /mods\/\d+/;
-  // urls.forEach((element) => {
-  //   element.split("/monsterhunterrise/");
-  //   if (regex.test(element[1])) {
-  //     let modId = element[1].split("mods/")[1];
-  //   }
-  // });
-}
 
-const url =
-  "https://www.nexusmods.com/Core/Libs/Common/Managers/Downloads?GenerateDownloadUrl";
-let fileId = 11361;
-const gameId = { monsterhunterrise: 4095 };
-const totalId = [];
-function connect(file_id, game_id) {
-  let params =
-    `fid=${file_id}` + `&game_id=${game_id}`;
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded"
-  );
-
-  xhr.onreadystatechange = function () {
-    if (
-      xhr.readyState === 4 &&
-      xhr.status === 200
-    ) {
-      // 请求成功，处理响应数据
-      const response = xhr.responseText;
-      console.log(response);
-
-      // 对响应数据进行处理，例如解析JSON数据
-      const jsonResponse = JSON.parse(response);
-    }
-  };
-  xhr.send(params);
-}
 
 function getPage(url) {
   const xhr = new XMLHttpRequest();
@@ -87,43 +45,81 @@ function getPage(url) {
         "#file-container-main-files"
       );
 
-      const optfiles = doc.querySelector(
-        "#file-container-optional-files"
-      );
+      console.log(url)
 
-      const node_list =
-        mainfiles.querySelectorAll(
+      if (mainfiles) {
+
+        const optfiles = doc.querySelector(
+          "#file-container-optional-files"
+        );
+
+        const idArr = new Array();
+        var eleArr = new Array();
+        if (mainfiles.querySelectorAll(
           ".btn.inline-flex.popup-btn-ajax"
-        );
-
-      const eleArr = Array.from(node_list);
-      const idArr = new Array();
-      const regex = /\?id=\d+/;
-
-      if (optfiles) {
-        optfiles
-          .querySelectorAll(
-            ".btn.inline-flex.popup-btn-ajax"
-          )
-          .forEach((element) => {
-            eleArr.push(element);
+        )) {
+          let node_list =
+            mainfiles.querySelectorAll(
+              ".btn.inline-flex.popup-btn-ajax"
+            );
+          let regex = /\?id=\d+/;
+          eleArr = Array.from(node_list);
+          if (optfiles) {
+            optfiles
+              .querySelectorAll(
+                ".btn.inline-flex.popup-btn-ajax"
+              )
+              .forEach((element) => {
+                eleArr.push(element);
+              });
+          }
+          eleArr.forEach((element) => {
+            let matchResult =
+              element.href.match(regex);
+            idArr.push(
+              matchResult[0].split("?id=")[1]
+            );
           });
-      }
-      eleArr.forEach((element) => {
-        let matchResult =
-          element.href.match(regex);
-        idArr.push(
-          matchResult[0].split("?id=")[1]
-        );
-      });
+        }
 
-      const result = [...new Set(idArr)];
-      console.log(result);
-      totalId.concat(result);
-      // const index = html.indexOf("Old files");
-      // if (index > 0) {
-      //   const result = html.substring(0, index);
-      // }
+        else {
+          let node_list =
+            mainfiles.querySelectorAll(
+              ".btn.inline-flex"
+            );
+          let regex = /\&file_id=\d+/;
+          eleArr = Array.from(node_list);
+          if (optfiles) {
+            optfiles
+              .querySelectorAll(
+                ".btn.inline-flex"
+              )
+              .forEach((element) => {
+                eleArr.push(element);
+              });
+          }
+          eleArr.forEach((element) => {
+            let matchResult =
+              element.href.match(regex);
+            idArr.push(
+              matchResult[0].split("&file_id=")[1]
+            );
+          });
+        }
+
+        const result = [...new Set(idArr)];
+
+        console.log(result);
+
+        chrome.runtime.sendMessage(
+          {
+            action: "download",
+            data: result
+          },
+        );
+      }
+      else
+        console.log("not a compatible page")
     } else {
       // 请求失败，处理错误
       console.error("请求失败：", xhr.status);
